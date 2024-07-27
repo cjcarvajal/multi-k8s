5da06b8c86c1df211178087da56cc2cc821f8659
Carlos Carvajal <ing.carlosj@gmail.com>
2024-07-27 17:14:22 -0500

    Add some log checkpoints for debug

--- worker/index.js
+++ worker/index.js
@@ -10,10 +10,12 @@ const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
+  console.log('To be calculated ',fib(index - 1) + fib(index - 2));
   return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
+  console.log('Message received ',message);
   redisClient.hset('values', message, fib(parseInt(message)));
 });
 sub.subscribe('insert');
